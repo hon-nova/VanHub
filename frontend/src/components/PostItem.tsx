de41b1e84e3af0e69138b9697dbd8270ba1df722
hon-nova <hon.tnguyen294@gmail.com>
2024-12-05 03:21:10 -0800

    cleaned posts

--- frontend/src/components/PostItem.tsx
+++ frontend/src/components/PostItem.tsx
@@ -1,70 +1,105 @@
-import '../styles/css/post-item-style.css'
-import { Post } from '../../../backend/src/shared/interfaces/index'
-import { Link } from 'react-router-dom'
+import "../styles/css/post-item-style.css";
+import { Post } from "../../../backend/src/shared/interfaces/index";
+import { Link } from "react-router-dom";
 
 interface PostItemProps {
-	post: Post;
-	onDelete: (id: number) => void;
-	onEdit: (id: number) => void;
-
+  post: Post;
+  onDelete: (id: number) => void;
+  onEdit: (id: number) => void;
 }
-const PostItem: React.FC<PostItemProps> =({post,onEdit,onDelete})=>{
-	let hostname =''
-	try {
-		hostname = new URL(post?.link).hostname || 'N/A'
-	} catch(error){
-		console.error(`error @PostItem: `,error)
-	}
-	return (
-		<div className="d-flex justify-content-between post-item">
-			<div>
-				<span><a href={post?.link}  className="mx-2" target="_blank" rel="noreferrer"><b>{post?.title}</b></a>({hostname})</span>
-				<span><p>By <i>{post?.creator} at {post?.timestamp}</i></p></span>
-			</div>
-			<div className="">				
-				<Link 
-					to={`/public/posts/edit/${post.id}`}
-					state= {{ post }}><i className="bi bi-pencil mx-2"></i>Edit</Link>
-				{/* <button onClick={()=>onDelete(post.id)}><i className="bi bi-trash mx-2"></i>Delete</button> */}
-				{/* button to trigger the modal */}
-				<button type="button" className="btn btn-primary" 
-				data-bs-toggle="modal" 
-				
-				data-bs-target={`#deleteModal-${post.id}`}
-				><i className="bi bi-trash mx-2"></i>
-  					Delete
-				</button>
-				{/* Modal */}				
-				<div 
-					className="modal fade" 					
-					id={`deleteModal-${post.id}`}
-					 data-bs-backdrop="static" data-bs-keyboard="false" 
-					 aria-labelledby={`deleteModalLabel-${post.id}`}
-					 aria-hidden="true">
-					<div className="modal-dialog">
-						<div className="modal-content">
-							<div className="modal-header">
-							<h1 className="modal-title fs-5" 
-							id={`deleteModalLabel-${post.id}`}
-							>Are you sure you'd want to delete this post?</h1>
-							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-							</div>
-							{/* <div className="modal-body">
-							...
-							</div> */}
-							<div className="modal-footer">
-								<a href={`/public/posts`} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</a> 								
-								<button 
-								onClick={()=>onDelete(post.id)}
-								type="button" className="btn btn-danger"
-								data-bs-dismiss="modal">Delete</button>								      
-							</div>
-						</div>
-					</div>
-				</div>
-				{/* end modal */}				
-			</div>
-		</div>
-	)
-}
+const PostItem: React.FC<PostItemProps> = ({ post, onEdit, onDelete }) => {
+  let hostname = "";
+  try {
+    hostname = new URL(post?.link).hostname || "N/A";
+  } catch (error) {
+    console.error(`error @PostItem: `, error);
+  }
+  return (
+    <div className="d-flex justify-content-between post-item">
+      <div>
+        <span>
+          <a
+            href={post?.link}
+            className="mx-2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <b>{post?.title}</b>
+          </a>
+          ({hostname})
+        </span>
+        <span>
+          <p>
+            By{" "}
+            <i>
+              {post?.creator} at {post?.timestamp}
+            </i>
+          </p>
+        </span>
+      </div>
+      <div className="">
+        <Link to={`/public/posts/edit/${post.id}`} state={{ post }}>
+          <i className="bi bi-pencil mx-2"></i>Edit
+        </Link>
+        {/* button to trigger the modal */}
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target={`#deleteModal-${post.id}`}
+        >
+          <i className="bi bi-trash mx-2"></i>
+          Delete
+        </button>
+        {/* Modal */}
+        <div
+          className="modal fade"
+          id={`deleteModal-${post.id}`}
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          aria-labelledby={`deleteModalLabel-${post.id}`}
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1
+                  className="modal-title fs-5"
+                  id={`deleteModalLabel-${post.id}`}
+                >
+                  Are you sure you'd want to delete this post?
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>              
+              <div className="modal-footer">
+                <a
+                  href={`/public/posts`}
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Cancel
+                </a>
+                <button
+                  onClick={() => onDelete(post.id)}
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end modal */}
+      </div>
+    </div>
+  );
+};
 export default PostItem;
