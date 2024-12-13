4e39d8ac93bf3898235d2191152e781ebafecbf8
hon-nova <hon.tnguyen294@gmail.com>
2024-12-13 12:21:34 -0800

    successfully uploaded avatar on supabase

--- backend/src/db/supabaseClient.ts
+++ backend/src/db/supabaseClient.ts
@@ -4,8 +4,15 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseURL = process.env.SUPABASE_URL as string;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
 const accessToken=process.env.SUPABASE_ACCESS_TOKEN as string
+const supabaseServiceRoleKey=process.env.SUPABASE_SERVICE_ROLE_KEY as string
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2dmRleHB4dGVzdXlqbm9kc2xqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzAzMjMzOSwiZXhwIjoyMDQ4NjA4MzM5fQ.BUELlzm3AsOnjcdV14uF5jdLGf0jsQFT23bmGITfXXE
 
-export const supabase = createClient(supabaseURL, supabaseAnonKey);
+// (async()=>{
+// 	console.log(`supabaseServiceRoleKey: `, supabaseServiceRoleKey)
+// })()
+// export const supabase = createClient(supabaseURL, supabaseAnonKey);
+export const supabase = createClient(supabaseURL, supabaseServiceRoleKey);
+
 // headers: { Authorization: `Bearer ${USER_ACCESS_TOKEN}` },
 // export const createSupabaseClient = (accessToken: string) => {
 // 	return createClient(supabaseURL, supabaseAnonKey, {
