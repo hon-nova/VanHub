7fd35f3af07e883a7d2c61add04e428a20730368
hon-nova <hon.tnguyen294@gmail.com>
2024-12-15 19:50:27 -0800

    updated keys

--- backend/src/db/supabaseClient.ts
+++ backend/src/db/supabaseClient.ts
@@ -3,8 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseURL = process.env.SUPABASE_URL as string;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
-const accessToken=process.env.SUPABASE_ACCESS_TOKEN as string
+const accessToken=process.env.ACCESS_TOKEN as string
 const supabaseServiceRoleKey=process.env.SUPABASE_SERVICE_ROLE_KEY as string
+
+/**
+ * REACT_APP_SUPABASE_URL=https://pvvdexpxtesuyjnodslj.supabase.co
+REACT_APP_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2dmRleHB4dGVzdXlqbm9kc2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwMzIzMzksImV4cCI6MjA0ODYwODMzOX0.iRmuj85ZS0Lw_Arof_VKVR7cN3IlbU4hIY2q_cAKOws
+REACT_APP_SUPABASE_SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2dmRleHB4dGVzdXlqbm9kc2xqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzAzMjMzOSwiZXhwIjoyMDQ4NjA4MzM5fQ.BUELlzm3AsOnjcdV14uF5jdLGf0jsQFT23bmGITfXXE
+ */
 
 export const supabase = createClient(supabaseURL, supabaseServiceRoleKey);
 
